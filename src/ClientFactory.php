891f0fd117072a6122bee735d11b4178264e287a
Andi Rückauer <arueckauer@gmail.com>
2018-12-07 15:25:31 +0100

    Implemented zend service-manager factory for Client

---
+++ src/ClientFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace arueckauer\Harvest;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class ClientFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param array|null $options
+     * @return Client
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $harvestConfig = $container->has('config') ? $container->get('config') : [];
+        $headers       = $harvestConfig['headers'] ?? [];
+        $config        = $harvestConfig['config']   ?? [];
+
+        return new Client($headers, $config);
+    }
+}
